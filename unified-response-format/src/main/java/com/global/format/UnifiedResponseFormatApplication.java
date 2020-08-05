612e337eeaf320cd6cc5e2637f813d6e5d14c886
yangyu <yangyu80@aiyun.com>
2020-08-05 23:13:28 +0800

    统一响应格式封装

---
+++ unified-response-format/src/main/java/com/global/format/UnifiedResponseFormatApplication.java
@@ -0,0 +1,13 @@
+package com.global.format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedResponseFormatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnifiedResponseFormatApplication.class, args);
+    }
+
+}
